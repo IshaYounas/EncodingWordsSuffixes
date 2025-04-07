96b3410f9e84e5bec0ae21836654ba834f5f92e2
ISHA YOUNAS <G00427804@atu.ie>
2025-04-07 15:09:33 +0100

    Created a LoadFile.java to load the csv file

--- EncodingWordsSuffixes/src/ie/atu/sw/LoadFile.java
+++ EncodingWordsSuffixes/src/ie/atu/sw/LoadFile.java
@@ -1,5 +1,35 @@
 package ie.atu.sw;
 
-public class LoadFile {
+// imports
+import java.util.Map;
+import java.util.TreeMap;
+import java.io.*; // file imports
 
-}
+public class LoadFile 
+{
+	// instance variable
+	private Map<String, Integer> m = new TreeMap<>(); // type string & integer
+	
+	// custom methods
+	public void parse(String file)
+	{
+		try (var in = new BufferedInputStream(new FileInputStream(new File (file))))
+		{
+			var line = "";
+			
+			// read text using buffered reader
+			BufferedReader read = new  BufferedReader(new InputStreamReader(in)); 
+			
+			while ((line = read.readLine()) != null)
+			{
+				String[] pair = line.split(","); // splitting by comma
+				m.put(pair[0], Integer.parseInt(pair[1])); // storing in a tree map
+			} // while
+		} // try
+		
+		catch (Exception e) 
+		{
+			System.err.println("Error: " + e.getMessage());
+		} // catch
+	} // parse
+} // class
